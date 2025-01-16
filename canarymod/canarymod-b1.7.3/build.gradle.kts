fc706c3e9afabe6cfc159a17eaec2b52a59e7365
SprainedSpark89 <87820585+SprainedSpark89@users.noreply.github.com>
2025-01-16 15:59:58 -0800

    Update build.gradle.kts

--- canarymod/canarymod-b1.7.3/build.gradle.kts
+++ canarymod/canarymod-b1.7.3/build.gradle.kts
@@ -3,10 +3,10 @@ base {
 }
 
 dependencies {
-    compileOnly("net.canarymod:CanaryMod-b1.7.3")
-	compileOnly("jar.jar:jarjar-b1.7.3")
-	compileOnly("net.minecraft:minecraft_servero-b1.7.3")
-	compileOnly("security.hell:mysql-connector-java-bin-b1.7.3")
+    compileOnly(":CanaryMod-b1.7.3")
+	compileOnly(":jarjar-b1.7.3")
+	compileOnly(":minecraft_servero-b1.7.3")
+	compileOnly(":mysql-connector-java-bin-b1.7.3")
     compileOnly(project(":api"))
     compileOnly(project(":common"))
     compileOnly(project(":loader"))
